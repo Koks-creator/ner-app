cf349b2254e57be3baa4cf0698fd0cfbceea9d9d
Koks-creator <73878161+Koks-creator@users.noreply.github.com>
2025-08-04 21:30:55 +0200

    Update forms.py

--- web_app/forms.py
+++ web_app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, SubmitField
 from flask_wtf.file import MultipleFileField, FileAllowed
-from wtforms.validators import DataRequired, ValidationError
+from wtforms.validators import ValidationError, length
 
 from config import Config
 
@@ -14,12 +14,41 @@ def max_files_count(max_count: int):
     return _max_files_count
 
 
+def max_files_size(max_size_mb: int):
+    max_size_bytes = max_size_mb * 1024 * 1024  # Konwersja MB na bajty
+    
+    def _validate(form, field):
+        files = field.data
+        if not files or len(files) == 0:
+            return
+        
+        total_size = 0
+        for f in files:
+            if f:
+                current_position = f.tell()
+                
+                f.seek(0, 2)
+                file_size = f.tell()
+                total_size += file_size
+                
+                f.seek(current_position)
+        
+        if total_size > max_size_bytes:
+            raise ValidationError(
+                f"Combined size of all files cannot be larger than {max_size_mb} MB. Current size:  {total_size / (1024 * 1024):.2f} MB."
+            )
+    
+    return _validate
+
 class MainForm(FlaskForm):
     text_area = TextAreaField("Paste your text here",
-                              render_kw={"rows": 15}
+                              [length(max=10000)],
+                              render_kw={"rows": 15},
                               )
     file_input = MultipleFileField("Upload files",
-                                   validators=[FileAllowed(["txt"]), max_files_count(Config.WEB_APP_MAX_FILES)],
+                                   validators=[FileAllowed(["txt"]),
+                                               max_files_count(Config.WEB_APP_MAX_FILES),
+                                               max_files_size(Config.WEB_APP_MAX_FILES_SIZE_MB)],
                                    )
 
     submit = SubmitField("Submit")
